67b29590c34f806a30f5756bf0bf27aea44275a3
Roy Elzur <relzur@broadinstitute.org>
2021-03-05 18:24:12 -0500

    power calculation includes QCed out variants in finemapped regions, and some other small changes

--- eval_meta_finemapping.py
+++ eval_meta_finemapping.py
@@ -1,3 +1,5 @@
+"""Computes power and calibration for a single meta-analysis configuration."""
+
 import numpy as np
 import pandas as pd
 import argparse
@@ -5,18 +7,24 @@ import sys
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--config", action="store", type=int, required=True)
-parser.add_argument("--nbins", action="store", type=int, required=True, dest="n_bins")
 args = parser.parse_args()
 
-n_bins = args.n_bins
 config = args.config
+
+bins = [0, 0.01, 0.1, 0.5, 0.9, 1.1]
+n_bins = len(bins) - 1
 path_prefix = "/humgen/atgu1/methods/mkanai/workspace/202011_meta_finemapping"
 n_phenos = 100
 
 sum_pips = [0] * n_bins
 n_causals = [0] * n_bins
 bin_counts = [0] * n_bins
-# total number of simulated causal SNPs
+
+all_pips = np.array([])
+causal_status = np.array([], dtype=bool)
+
+# total number of simulated causal SNPs, including QCed out variants,
+# excluding variants in regions not fine-mapped
 n_total_causals = 0
 n_total_snps = 0
 
@@ -30,44 +38,47 @@ for pheno in range(1, n_phenos+1):
     try:
         fm_results = pd.read_table(
             f"{path_prefix}/ABF/meta.pheno{pheno}.config{config}.ABF.snp.bgz", 
-            usecols=["rsid", "prob"], 
+            usecols=["rsid", "prob", "region"], 
             compression="gzip"
         )
     except FileNotFoundError:
         print(f".snp.bgz file not found for pheno={pheno}, config={config}. Skipping.")
         continue
+
+    # only count causal variants that are in fine-mapped regions
+    finemapped_regions = fm_results["region"].unique().tolist()
+    for v in true_beta["variant"]:
+        for region in finemapped_regions:
+            chromosome = region.split(":")[0]
+            start, end = tuple([int(i) for i in region.split(":")[1].split("-")])
+            if (v.split(":")[0] == chromosome and 
+                int(v.split(":")[1]) >= start and
+                int(v.split(":")[1]) <= end):
+                n_total_causals += 1
+                break
     
-    pips = fm_results["prob"].to_numpy()
-    
-    n_total_causals += len(true_beta)
     n_total_snps += len(fm_results)
+    all_pips = np.append(all_pips, fm_results["prob"].to_numpy())
+    causal_status = np.append(causal_status, fm_results["rsid"].isin(true_beta.variant))
 
-    
-    # calculate calibration
-    bin_width = 1/n_bins
-    bins = np.arange(0, 1+bin_width, bin_width)
-    bins[-1] = 1.1  # to include PIP of 1 in top bin
-    for i in range(len(bins) - 1):
-        in_bin = np.logical_and(pips >= bins[i], pips < bins[i+1])
-        sum_pip_in_bin = fm_results.loc[in_bin, "prob"].sum()
-        n_causal_in_bin = fm_results.loc[in_bin, "rsid"].isin(true_beta.variant).sum()
-        bin_count_in_bin = in_bin.sum()
-        
-        sum_pips[i] += sum_pip_in_bin
-        n_causals[i] += n_causal_in_bin
-        bin_counts[i] += bin_count_in_bin
-        
+# calculate calibration
+for i in range(len(bins) - 1):
+    in_bin = np.logical_and(all_pips >= bins[i], all_pips < bins[i+1])
+    sum_pips[i] = all_pips[in_bin].sum()
+    n_causals[i] = causal_status[in_bin].sum()
+    bin_counts[i] = in_bin.sum()
+
 # calculate power 
+fractions = [0.0001, 0.0005, 0.001, 0.005, 0.01]
 powers = []
-for b in range(n_bins):
-    # can also divide by n_total_causals but I think that would be a less interpretable metric
-    power = sum(n_causals[b:]) / sum(n_causals) 
-    powers.append(power)
-
-# sanity check 
-# n finemapped causal variants <= total n causal variants (some missing due to not passing GWAS)
-assert sum(n_causals) <= n_total_causals
-# make sure binning was accurate
+sorted_causal = causal_status[np.argsort(all_pips)]
+for f in fractions:
+    # get number of causal SNPs in top fraction of PIPs
+    top_f = sorted_causal[-round(f*n_total_snps):].sum()
+    powers.append(top_f/n_total_causals)
+    
+# sanity checks
+assert causal_status.sum() <= n_total_causals
 assert sum(bin_counts) == n_total_snps
     
 # outut results
@@ -77,14 +88,16 @@ prop_causals = np.array(n_causals) / bin_counts
 powers = np.array(powers)
 
 df = pd.DataFrame({
+    "bin_left_edge": bins[:-1],
     "bin_count": bin_counts,
     "mean_pip": mean_pips,
     "prop_causal": prop_causals,
+    "power_fraction": fractions,
     "power": powers
 })
 
 df.to_csv(
     f"/broad/finucanelab/relzur/meta_analysis_fm/meta.config{config}.eval.txt", 
     sep="\t", 
-    index_label="bin"
+    index=False
 )
--- eval_meta_finemapping.sh
+++ eval_meta_finemapping.sh
@@ -2,12 +2,9 @@
 
 set -eu
 
-# default number of bins is 10
-nbins=${1:-10}
-
 for config in $(seq 16)
 do 
 
-python eval_meta_finemapping.py --config $config --nbins $nbins
+python eval_meta_finemapping.py --config $config
 
 done
